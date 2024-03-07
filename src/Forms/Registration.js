cd53950b728e65d5e65aaf542032e985af700ab0
nowshad-123 <133634131+nowshad-123@users.noreply.github.com>
2024-03-07 16:17:45 +0530

    updated the urls

--- src/Forms/Registration.js
+++ src/Forms/Registration.js
@@ -23,7 +23,7 @@ const Registration = () => {
     const handleOtp = async () => {
         setSentOtp(true);
 
-        const { data } = await axios.post("https://otp-back-iymp.onrender.com/users", {
+        const { data } = await axios.post("https://otp-back-iymp.onrender.com/sendingOtp", {
             "firstName": firstName,
             "phone": number
         });
@@ -35,7 +35,7 @@ const Registration = () => {
         // Disable the button after clicking
         setVerifyClicked(true);
 
-        const { data } = await axios.post('https://otp-back-iymp.onrender.com/verify', {
+        const { data } = await axios.post('https://otp-back-iymp.onrender.com/verifyingOtp', {
             "firstName": firstName,
             "phone": number,
             "otpCode": otpCode
